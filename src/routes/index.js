610dcde8930bf9801fecc4a39ab9850bd1ba976c
Netthor <joaofsn@dcomp.ufs.br>
2020-06-23 16:47:26 -0300

    seleção de links

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import search from '../pages/searchPage/index'
 import response from '../pages/responsePage/index'
+import teste from '../pages/testes/index'
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={search} />
             <Route exact path="/res" component={response} />
+            <Route exact path="/teste" component={teste} />
         </Switch>
     </BrowserRouter>
 )
